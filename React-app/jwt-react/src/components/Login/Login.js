0bc0f9315a3c1b8dc9d94a83ae51e342bef1d113
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2024-02-13 23:16:43 +0700

    update

--- React-app/jwt-react/src/components/Login/Login.js
+++ React-app/jwt-react/src/components/Login/Login.js
@@ -5,11 +5,11 @@ const Login = (props) => {
     <div className="login-container mt-3">
       <div className="container">
         <div className="row">
-          <div className="content-left col-7 d-">
+          <div className="content-left col-7 d-none d-sm-block">
             <div className="brand">Login Form</div>
             <div className="detail">Learning React & Nodejs</div>
           </div>
-          <div className="content-right col-5 green d-flex flex-column gap-3 py-3">
+          <div className="content-right col-12 green d-flex flex-column gap-3 py-3 col-sm-5">
             <input
               type="text"
               className="form-control"
@@ -21,7 +21,7 @@ const Login = (props) => {
               placeholder="Password"
             />
             <button className="btn btn-primary">Login</button>
-            <span className="text-center">Forgotten password ?</span>
+            <span className="mx-auto">Forgotten password ?</span>
             <hr />
             <div className="text-center">
               <button className="btn btn-success">Create Your Account</button>
