70bfc3573e3c0875db98bfce4d0b4f084a0a286d
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2024-02-20 21:14:23 +0700

    udate

--- React-app/jwt-react/src/components/Register/Register.js
+++ React-app/jwt-react/src/components/Register/Register.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-
+import { registerNewUser } from "../../services/userService";
 const Register = (props) => {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
@@ -63,17 +63,18 @@ const Register = (props) => {
     return true;
   };
 
-  const handleRegister = () => {
+  const handleRegister = async () => {
     let check = isValidInputs();
 
     if (check === true) {
-      axios.post("http://localhost:8081/api/v1/register", {
-        email,
-        phone,
-        username,
-        password,
-      });
-      toast.success("congratulations");
+      let response = await registerNewUser(email, phone, username, password);
+      let serverData = response.data;
+      if (+serverData.EC === 0) {
+        toast.success(serverData.EM);
+        history.push("/login");
+      } else {
+        toast.error(serverData.EM);
+      }
     }
   };
 
